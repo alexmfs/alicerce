b10c12333ef25de4893bbcead6fb03acabec5efa
Alex Machado <alex_mfarias@hotmail.com>
2014-04-28 22:03:01 -0300

    alteração index.php

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <?php
 //Header( 'HTTP/1.1 301 Moved Permanently' );
-<<<<<<< HEAD
  Header( 'Location: public' );
 ?>
-=======
- Header( 'Location: public/' );
-?>
->>>>>>> 25d2279775dc78ef28df470e25a4e571b0112dab
