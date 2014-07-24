a0d2bf29a8e61e49ca741be78b85c8200fe98a2a
Alex Machado <alex_mfarias@hotmail.com>
2014-07-24 10:42:31 -0300

    colors :: Aplicado shadow para cores que foram calculadas

---
+++ public/includes/helpers/types.php
@@ -0,0 +1,6 @@
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, quibusdam. Ad molestias maxime provident natus, nihil, nesciunt eos aut atque nobis rerum sunt doloremque temporibus dolor deleniti ipsam. Delectus, maxime.
+</p>
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, quibusdam. Ad molestias maxime provident natus, nihil, nesciunt eos aut atque nobis rerum sunt doloremque temporibus dolor deleniti ipsam. Delectus, maxime.
+</p>
