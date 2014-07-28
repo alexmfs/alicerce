c33c61aeffb98ea17f73f0d033a2744f36bf810e
Alex Machado <alex_mfarias@hotmail.com>
2014-07-28 15:18:46 -0300

    [if lt IE 7] :: browsehappy

--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
 <?php include ("includes/head.php"); ?>
+	<!--[if lt IE 7]>
+	<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+	<![endif]-->
 
 	<!-- Header -->
 	<header class="principal" role="banner">
