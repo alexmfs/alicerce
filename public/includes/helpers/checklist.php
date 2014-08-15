7eb435c703a33b7370f3c32fdb857d19fe1a05b2
Alex Machado <alex_mfarias@hotmail.com>
2014-08-15 13:13:23 -0300

    inclusao de grid separado do semantic.gs

---
+++ public/includes/helpers/checklist.php
@@ -0,0 +1,145 @@
+<div class="wrap">
+	<form action="#" method="post">
+		<h1 class="hc">Checklist</h1>
+
+		<div class="desktop-12">
+			<h2 class="hc">Global</h2>
+		</div>
+		<div class="checkbox">
+			<div class="desktop-2">
+				<input id="" type="checkbox" name="global" value="" checked>
+				<label for="">_colors.less</label>
+			</div>
+			<div class="desktop-2">
+				<input id="" type="checkbox" name="global" value="" checked>
+				<label for="">_types.less</label>
+			</div>
+			<div class="desktop-2">
+				<input id="" type="checkbox" name="global" value="" checked>
+				<label for="">_grid.less</label>
+			</div>
+			<div class="desktop-2">
+				<input id="" type="checkbox" name="global" value="">
+				<label for="">_animations.less</label>
+			</div>
+			<div class="desktop-2">
+				<input id="" type="checkbox" name="global" value="">
+				<label for="">_visibility.less</label>
+			</div>
+		</div>
+
+		<div class="desktop-12">
+			<h2 class="hc">Atoms</h2>
+		</div>
+		<div class="checkbox">
+			<div class="desktop-2">
+				<input id="" type="checkbox" name="atoms" value="" checked>
+				<label for="">buttons.less</label>
+			</div>
+			<div class="desktop-2">
+				<input id="" type="checkbox" name="atoms" value="" checked>
+				<label for="">dropdown.less</label>
+			</div>
+			<div class="desktop-2">
+				<input id="" type="checkbox" name="atoms" value="">
+				<label for="">links.less</label>
+			</div>
+			<div class="desktop-2">
+				<input id="" type="checkbox" name="atoms" value="">
+				<label for="">inlines.less</label>
+			</div>
+			<div class="desktop-2">
+				<input id="" type="checkbox" name="atoms" value="" checked>
+				<label for="">tooltips.less</label>
+			</div>
+			<div class="desktop-2">
+				<input id="" type="checkbox" name="atoms" value="" checked>
+				<label for="">inputs.less</label>
+			</div>
+			<div class="desktop-2">
+				<input id="" type="checkbox" name="atoms" value="">
+				<label for="">titles.less</label>
+			</div>
+		</div>
+
+		<div class="desktop-12">
+			<h2 class="hc">Molecules</h2>
+		</div>
+		<div class="checkbox">
+			<div class="desktop-2">
+				<input id="" type="checkbox" name="molecules" value="" checked>
+				<label for="">video.less</label>
+			</div>
+			<div class="desktop-2">
+				<input id="" type="checkbox" name="molecules" value="">
+				<label for="">slider.less</label>
+			</div>
+			<div class="desktop-2">
+				<input id="" type="checkbox" name="molecules" value="" checked>
+				<label for="">messages.less</label>
+			</div>
+			<div class="desktop-2">
+				<input id="" type="checkbox" name="molecules" value="" checked>
+				<label for="">modal.less</label>
+			</div>
+			<div class="desktop-2">
+				<input id="" type="checkbox" name="molecules" value="" checked>
+				<label for="">tabs.less</label>
+			</div>
+			<div class="desktop-2">
+				<input id="" type="checkbox" name="molecules" value="">
+				<label for="">navigation.less</label>
+			</div>
+			<div class="desktop-2">
+				<input id="" type="checkbox" name="molecules" value="" checked>
+				<label for="">breadcrumb.less</label>
+			</div>
+			<div class="desktop-2">
+				<input id="" type="checkbox" name="molecules" value="">
+				<label for="">pagination.less</label>
+			</div>
+			<div class="desktop-2">
+				<input id="" type="checkbox" name="molecules" value="">
+				<label for="">headings.less</label>
+			</div>
+			<div class="desktop-2">
+				<input id="" type="checkbox" name="molecules" value="">
+				<label for="">search-bar.less</label>
+			</div>
+			<div class="desktop-2">
+				<input id="" type="checkbox" name="molecules" value="">
+				<label for="">produto.less</label>
+			</div>
+		</div>
+
+		<div class="desktop-12">
+			<h2 class="hc">Organisms</h2>
+		</div>
+		<div class="checkbox">
+		<div class="desktop-2">
+			<input id="" type="checkbox" name="organisms" value="">
+			<label for="">header.less</label>
+		</div>
+		<div class="desktop-2">
+			<input id="" type="checkbox" name="organisms" value="">
+			<label for="">footer.less</label>
+		</div>
+	</form>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ public/includes/helpers/grid.php
@@ -0,0 +1,90 @@
+<section id="hc-tooltips">
+	<header class="desktop-12">
+		<h1 class="hc">Grid <small>Aplicação de Classes</small></h1>
+	</header>
+	<div class="desktop-12 tablet-12 mobile-12">
+		<p class="shape visible-desktop">12</p>
+		<p class="shape visible-tablet">12</p>
+		<p class="shape visible-mobile">12</p>
+	</div>
+	<div class="desktop-11 tablet-11 mobile-11">
+		<p class="shape visible-desktop">11</p>
+		<p class="shape visible-tablet">11</p>
+		<p class="shape visible-mobile">11</p>
+	</div>
+	<div class="desktop-1 tablet-1 mobile-1">
+		<p class="shape visible-desktop">1</p>
+		<p class="shape visible-tablet">1</p>
+		<p class="shape visible-mobile">1</p>
+	</div>
+	<div class="desktop-10 tablet-10 mobile-10">
+		<p class="shape visible-desktop">10</p>
+		<p class="shape visible-tablet">10</p>
+		<p class="shape visible-mobile">10</p>
+	</div>
+	<div class="desktop-2 tablet-2 mobile-2">
+		<p class="shape visible-desktop">2</p>
+		<p class="shape visible-tablet">2</p>
+		<p class="shape visible-mobile">2</p>
+	</div>
+	<div class="desktop-9 tablet-9 mobile-9">
+		<p class="shape visible-desktop">9</p>
+		<p class="shape visible-tablet">9</p>
+		<p class="shape visible-mobile">9</p>
+	</div>
+	<div class="desktop-3 tablet-3 mobile-3">
+		<p class="shape visible-desktop">3</p>
+		<p class="shape visible-tablet">3</p>
+		<p class="shape visible-mobile">3</p>
+	</div>
+	<div class="desktop-8 tablet-8 mobile-8">
+		<p class="shape visible-desktop">8</p>
+		<p class="shape visible-tablet">8</p>
+		<p class="shape visible-mobile">8</p>
+	</div>
+	<div class="desktop-4 tablet-4 mobile-4">
+		<p class="shape visible-desktop">4</p>
+		<p class="shape visible-tablet">4</p>
+		<p class="shape visible-mobile">4</p>
+	</div>
+	<div class="desktop-7 tablet-7 mobile-7">
+		<p class="shape visible-desktop">7</p>
+		<p class="shape visible-tablet">7</p>
+		<p class="shape visible-mobile">7</p>
+	</div>
+	<div class="desktop-5 tablet-5 mobile-5">
+		<p class="shape visible-desktop">5</p>
+		<p class="shape visible-tablet">5</p>
+		<p class="shape visible-mobile">5</p>
+	</div>
+	<div class="desktop-6 tablet-6 mobile-6">
+		<p class="shape visible-desktop">6</p>
+		<p class="shape visible-tablet">6</p>
+		<p class="shape visible-mobile">6</p>
+	</div>
+	<div class="desktop-6 tablet-6 mobile-6">
+		<p class="shape visible-desktop">6</p>
+		<p class="shape visible-tablet">6</p>
+		<p class="shape visible-mobile">6</p>
+	</div>
+	<div class="desktop-4 tablet-4 mobile-4">
+		<p class="shape visible-desktop">4</p>
+		<p class="shape visible-tablet">4</p>
+		<p class="shape visible-mobile">4</p>
+	</div>
+	<div class="desktop-4 tablet-4 mobile-4">
+		<p class="shape visible-desktop">4</p>
+		<p class="shape visible-tablet">4</p>
+		<p class="shape visible-mobile">4</p>
+	</div>
+	<div class="desktop-2 tablet-2 mobile-2">
+		<p class="shape visible-desktop">2</p>
+		<p class="shape visible-tablet">2</p>
+		<p class="shape visible-mobile">2</p>
+	</div>
+	<div class="desktop-2 tablet-2 mobile-2">
+		<p class="shape visible-desktop">2</p>
+		<p class="shape visible-tablet">2</p>
+		<p class="shape visible-mobile">2</p>
+	</div>
+</section>
