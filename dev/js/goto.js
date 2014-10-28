23e66f3f3746cee3e713bd44dc7cbb204b5864c7
Alex Machado <alex_mfarias@hotmail.com>
2014-10-28 11:55:16 -0200

    Update goto.js

--- dev/js/goto.js
+++ dev/js/goto.js
@@ -2,7 +2,7 @@
 	$('.goto').on('click', function(){
 		var to = $(this).attr('href'); // $(this) is the clicked link. We store its href.
 		$('html, body').animate({ scrollTop: ($(to).offset().top)-90}, 700);
-		$('nav a').removeClass("active");
+		$('.goto').removeClass("active");
 		$(this).addClass("active");
 		return false;
 	});
