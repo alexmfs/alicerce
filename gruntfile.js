c40ee8d270e7e99753a3190f21171d84f3bfa7fb
Alex Machado <alex_mfarias@hotmail.com>
2014-11-12 19:51:52 -0200

    Update gruntfile.js

--- gruntfile.js
+++ gruntfile.js
@@ -82,5 +82,5 @@ module.exports = function(grunt) {
 	});
 	// registrando tarefa default
 	grunt.registerTask( 'default', ['watch'] );
-	grunt.registerTask( 'c', ['copy'] );
+	grunt.registerTask( 'initial', ['less','cmq','cssmin','concat'] );
 };
