88bd8bae4899f6c6eee1b529647a843fe1af6d6b
Alex Machado <alex_mfarias@hotmail.com>
2014-12-02 10:55:59 -0200

    Create dropdown.js

---
+++ dev/js/dropdown.js
@@ -0,0 +1,18 @@
+$('.drop-down').on('click', function () {
+  if ($(window).width() < 800) {
+    // $('.drop-down').toggleClass('close');
+    // $('.drop-list').removeClass('drop-active');
+    // $(this).next('.drop-list').toggleClass('drop-active');
+    $('.drop-list').removeClass('drop-active');
+    $(this).next('.drop-list').addClass('drop-active');
+
+    if (!$(this).hasClass('close')) {
+      $('.drop-down').removeClass('close');
+      $(this).addClass('close');
+    } else {
+      $(this).removeClass('close');
+      $('.drop-list').removeClass('drop-active');
+    }
+    return false;
+  }
+});
