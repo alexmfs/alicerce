d02af363d9e746540c44defa400badeea008acad
Alex Machado <alex_mfarias@hotmail.com>
2014-12-08 20:18:48 -0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,6 +1,7 @@
 rm -rf public/.git
-bower install && npm install && grunt initial
+bower install && npm install
 git add .
 git commit -a -m "dependecias instaladas"
+git push origin master
 rm -rf start.bat
 grunt
