cd949090ed83c0a7c0c7eed4000022b70e59d370
Alex Machado <alex_mfarias@hotmail.com>
2015-01-06 09:04:57 -0200

    tabs.js :: jshint

--- dev/js/tabs.js
+++ dev/js/tabs.js
@@ -1,23 +1,22 @@
-  //////////////////////////
-  // Abas
-  //////////////////////////
-
+function tabs(tab) {
   /* Guarda IDs dos elementos */
-  var abas = '.abas';
-  var conteudo = 'ul.conteudo';
+  var nav = tab + ' .tabs-nav';
+  var content = tab + ' .tabs-content';
   /* Oculta todas as abas */
-  $(conteudo + ' li').hide();
+  $(content + ' li').hide();
   /* Exibe a primeira aba */
-  $(conteudo + ' li:first-child').show();
+  $(content + ' li:first-child').show();
   /* Quando uma aba for clicada */
-  $(abas + ' a').click(function () {
+  $(nav + ' a').click(function () {
     /* Remove todas as classes de todas as abas */
-    $(abas + ' a').removeClass('selected');
+    $(nav + ' a').removeClass('selected');
     /* Acrescenta uma classe CSS marcando visualmente a aba como selecionada */
     $(this).addClass('selected');
     /* Oculta todas as abas abertas */
-    $(conteudo + ' li').hide();
+    $(content + ' li').hide();
     /* Exibe a aba que foi clicada */
-    $(conteudo + ' ' + $(this).attr('href')).show();
+    $(content +  ' ' + $(this).attr('href')).show();
     return false;
   });
+}
+tabs('.tabs-namecustom');
