d1738b484f93aac6b62163ae883234f7829cbfc1
Alex Machado <alex_mfarias@hotmail.com>
2017-05-16 18:06:57 +0000

    index.php alterados online com o Bitbucket

--- index.php
+++ index.php
@@ -55,6 +55,11 @@
 <html lang="<?= $lang ?>">
 <head>
 	<meta charset="UTF-8">
+	
+	<meta name="theme-color" content="#e4034e">
+	<meta name="apple-mobile-web-app-status-bar-style" content="#e4034e">
+	<meta name="msapplication-navbutton-color" content="#e4034e">	
+	
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 	<link rel="stylesheet" href="dest/style.css">
 	<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
