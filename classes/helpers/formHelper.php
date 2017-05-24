153750a7e03fdf062ffedd22dc8c26392f58fb43
Alex Machado <alex_mfarias@hotmail.com>
2017-05-24 18:22:23 +0000

    formHelper.php criado online com Bitbucket

---
+++ classes/helpers/formHelper.php
@@ -0,0 +1,84 @@
+<?php
+
+function getForm($arr){
+
+    $options = 'class="form-control"';
+    $action = '';
+    $method = 'post';
+    
+    if(isset($value["action"])){
+        $action = $value["action"];
+    }
+    
+    if(isset($value["method"])){
+        $method = $value["method"];
+    }       
+
+    if (isset($arr["action"])) {
+        echo '<form action="'.$action.'" method="'.$method.'">';        
+    }
+
+    if (isset($arr["items"])) {
+
+        foreach($arr["items"] as $key => $value) {
+
+            $inputType = 'text';
+
+            if(isset($value["type"])){
+                $inputType = $value["type"];
+            }  
+
+            echo
+                "<div $options>",
+                    '<label for="'.strtolower($value['label']).'">'.$value['label']."</label>",
+                    '<input type="'.$inputType.'" id="'.strtolower($value['label']).'">',
+                "</div>";
+        }
+
+    }
+    echo
+        '<div class="form-control buttons">',
+            "<input type='submit'>",
+        "</div>";
+    
+    echo "</form>";
+    
+
+  
+}
+
+getForm([
+    
+    "action" => "send.php",
+    "method" => "post",
+
+    'items' => [
+
+        [
+            'label' => 'Nome'
+        ],
+        [
+            'label' => 'Email'
+        ],
+        [
+            'label' => 'Senha',
+            'type'  => 'password'
+        ],
+        [
+            'label' => 'Mensagem'
+        ],
+
+    ]
+
+]);
+
+?>
+
+<style>
+    .form-control{
+        margin-bottom: 10px;
+    }
+    input, label{
+        display: block;
+    }
+</style>
